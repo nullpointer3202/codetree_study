64fa64c0fa9a7d02af586a35e89a5d33116dee6d
sangki-song <126885150+nullpointer3202@users.noreply.github.com>
2026-03-28 23:56:47 +0900

    Title: 비교 연산, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260328/비교 연산/comparison-operator.py
+++ 260328/비교 연산/comparison-operator.py
@@ -1,11 +1,11 @@
-a = int(input())
-b = int(input())
+a, b = map(int, input().split())
 
-print(a >= b)
-print(a >= b)
-print(a >= b)
-print(a >= b)
-print(a >= b)
+print(int(a >= b))
+print(int(a > b))
+print(int(a <= b))
+print(int(a < b))
+print(int(a == b))
+print(int(a != b))
 
 
 # >=
